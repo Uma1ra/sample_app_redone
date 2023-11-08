89daadcad6abb482f95cba1b7cecb7e567325095
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-08 02:41:22 +0000

    [add] 画像添付のテスト（成功）

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,3 +1,11 @@
 require "rails_helper"
 
-describe ""
+RSpec.describe List, type: :model do
+  it "画像が添付されている" do
+    list = create(:list)
+
+    expect(list.image).to be_attached
+    expect(list.image).to be_a(ActiveStorage::Attached::One)
+    expect(list.image).to be_an_instance_of(ActiveStorage::Attached::One)
+  end
+end
