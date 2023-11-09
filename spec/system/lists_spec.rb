6e30191de27b42051d857d43f84eb08fb7f30ef2
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-09 03:26:55 +0000

    [add] 投稿画面の表示/投稿後のリダイレクト先テスト（成功）

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -18,6 +18,31 @@ describe "投稿のテスト" do
     end
   end
 
+  describe "投稿画面のテスト" do
+    before do
+      visit new_list_path
+    end
+    
+    context "表示の確認" do
+      it "new_list_pathが'/lists/new'であるか" do
+        expect(current_path).to eq("/lists/new")
+      end
+    end
+    it "投稿ボタンが表示されているか" do
+      expect(page).to have_button "投稿"
+    end
+    
+    context "投稿処理のテスト" do
+      it "投稿後のリダイレクト先は正しいか" do
+        fill_in "list[title]", with: Faker::Lorem.characters(number:5)
+        fill_in "list[body]", with: Faker::Lorem.characters(number:20)
+        attach_file("list[image]", "spec/support/test_no_image.jpg")
+        click_button "投稿"
+        expect(page).to have_current_path list_path(List.last) 
+      end
+    end
+  end
+
   # describe "" do
   #   context "" do
   #     it do
